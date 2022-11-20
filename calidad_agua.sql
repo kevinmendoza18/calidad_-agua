21a39167df9d59fa89c97ddf8d5d7ca9c726435a
kevin obed mendoza <kevin.obed1999@gmail.com>
2022-11-20 10:28:38 -0500

    add base de datos

---
+++ calidad_agua.sql
@@ -0,0 +1,36 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         5.7.33 - MySQL Community Server (GPL)
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Volcando estructura de base de datos para calidad_agua
+CREATE DATABASE IF NOT EXISTS `calidad_agua` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `calidad_agua`;
+
+-- Volcando estructura para tabla calidad_agua.datos
+CREATE TABLE IF NOT EXISTS `datos` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `muestra1` int(11) NOT NULL DEFAULT '0',
+  `muestra2` int(11) NOT NULL DEFAULT '0',
+  `muestra3` int(11) NOT NULL DEFAULT '0',
+  `muestra4` int(11) NOT NULL DEFAULT '0',
+  `fecha_muestra` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=96 DEFAULT CHARSET=utf8;
+
+-- La exportación de datos fue deseleccionada.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
